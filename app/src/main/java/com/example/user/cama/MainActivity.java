05e061177af4e8d91c45322ec8cef123cc72d473
Ram S <ramsh92@gmail.com>
2018-04-26 16:59:31 +0300

    ftdfd

--- app/src/main/java/com/example/user/cama/MainActivity.java
+++ app/src/main/java/com/example/user/cama/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +20,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+
+    }
+
+    public void newEventClick(View view){
+        Intent newEventCreate = new Intent(this, NewEventCreate.class);
+        startActivity(newEventCreate);
+        Button h=new Button(this);
     }
 
     @Override
